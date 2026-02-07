f03fd05d47b83410e3b0f5e5fa5dc4404c1f3f27
Ray Pauszek <ray.pauszek@gmail.com>
2026-02-07 12:14:32 +0100

    set up nunjucks with small toy example

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 #!/usr/bin/env node
-
+import nunjucks from "nunjucks";
+import path from "path";
 import something from "@render/test";
 
 console.log("Hello CV Generator 👋");
 something();
+
+
+const templatePath = path.resolve(__dirname, "./templates");
+nunjucks.configure(templatePath, {
+  autoescape: true
+});
+
+const html = nunjucks.render("hello.njk", {
+  title: "Hello Test",
+  name: "Ray",
+  subtitle: "CV Generator in Progress"
+});
+
+console.log(html);
+
