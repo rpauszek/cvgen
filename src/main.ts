ab07f391200591451bc2c490799428becd0382d6
Ray Pauszek <ray.pauszek@gmail.com>
2026-02-07 12:27:46 +0100

    add loadCvYaml

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
 import nunjucks from "nunjucks";
 import path from "path";
+import fs from "fs";
+import YAML from "yaml";
 import something from "@render/test";
+
+export function loadCvYaml<T>(cvPath: string): T {
+  const file = fs.readFileSync(cvPath, "utf-8");
+  return YAML.parse(file);
+}
 
 console.log("Hello CV Generator 👋");
 something();
 
+const examplePath = path.resolve(__dirname, "../example/cv.yml")
+console.log(examplePath)
+
+const data = loadCvYaml(examplePath);
+console.log(data)
 
 const templatePath = path.resolve(__dirname, "./templates");
 nunjucks.configure(templatePath, {
@@ -18,5 +30,5 @@ const html = nunjucks.render("hello.njk", {
   subtitle: "CV Generator in Progress"
 });
 
-console.log(html);
+// console.log(html);
 
